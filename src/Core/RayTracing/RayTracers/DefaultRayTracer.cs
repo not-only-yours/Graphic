4caff92cbd5e34c74a8c223a33ba17723bcfef2a
Yaroslaww-1 <yaroslaww1@gmail.com>
2022-06-24 23:29:14 +0300

    added ShadowRayTracer

---
+++ src/Core/RayTracing/RayTracers/DefaultRayTracer.cs
@@ -0,0 +1,35 @@
+﻿using Core.Geometry;
+using Core.Geometry.Shapes.Abstract;
+
+namespace Core.RayTracing.RayTracers;
+
+public class DefaultRayTracer : IRayTracer
+{
+    private readonly IEnumerable<Shape> _shapes;
+    private readonly Vector _lightSource;
+    
+    public DefaultRayTracer(IEnumerable<Shape> shapes, Vector lightSource)
+    {
+        _shapes = shapes;
+        _lightSource = lightSource;
+    }
+        
+    public TraceResult Trace(Point origin, Vector direction)
+    {
+        Intersection? nearestIntersection = null;
+        
+        foreach (var shape in _shapes)
+        {
+            var shapeIntersection = shape.GetIntersectionWith(origin, direction); //TODO: pass nearestIntersection to abort early inside GetIntersectionWith
+            
+            if (
+                shapeIntersection != null && 
+                (shapeIntersection.Distance < nearestIntersection?.Distance || nearestIntersection == null))
+            {
+                nearestIntersection = shapeIntersection;
+            }
+        }
+
+        return TraceResult.FromIntersectionAndLightSource(nearestIntersection, _lightSource);
+    }
+}
--- src/Core/RayTracing/TraceResult.cs
+++ src/Core/RayTracing/TraceResult.cs
@@ -20,11 +20,8 @@ public class TraceResult
 
     public static TraceResult FromIntersectionAndLightSource(Intersection? intersection, Vector lightSource)
     {
-        // cameraToPoint.GetUnitVector().Dot(_lightSource)
         if (intersection != null)
         {
-            // Console.WriteLine("Found " + intersection.Normal!.Dot(lightSource));
-            // return new TraceResult(intersection.Normal!.Dot(lightSource));
             return FromIntersection(intersection, lightSource);
         }
         else
@@ -36,7 +33,6 @@ public class TraceResult
     private static TraceResult FromIntersection(Intersection intersection, Vector lightSource)
     {
         var shading = (-lightSource).Dot(intersection.Normal);
-        // Console.WriteLine($"{-lightSource} * {intersection.Normal} = {shading}");
         return new TraceResult(intersection, shading); 
     }
 
@@ -45,10 +41,10 @@ public class TraceResult
         return new TraceResult(null, -3);
     }
 
-    // public static TraceResult FromInShadow(Intersection? intersection)
-    // {
-    //     return new TraceResult(intersection, 0);
-    // }
+    public static TraceResult FromInShadow(Intersection? intersection)
+    {
+        return new TraceResult(intersection, 0);
+    }
 
     public override string ToString()
     {
