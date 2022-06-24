45075270b5cc5f41e7484c38a23b79a0f0b5c1e8
Yaroslaww-1 <yaroslaww1@gmail.com>
2022-06-24 11:49:23 +0300

    refactored tracing: added TraceResults, changed Camera pixel<->point calculation

--- src/Core/RayTracing/ShapesWithLightSourceRayTracer.cs
+++ src/Core/RayTracing/ShapesWithLightSourceRayTracer.cs
@@ -1,45 +1,70 @@
 ﻿using Core.Geometry;
 using Core.Geometry.Shapes;
+using Core.Geometry.Shapes.Abstract;
 using Core.Scenery;
 
 namespace Core.RayTracing;
 
 public class ShapesWithLightSourceRayTracer : IRayTracer
 {
-    private readonly List<Shape> _shapes;
+    private readonly IEnumerable<Shape> _shapes;
     private readonly Camera _camera;
     private readonly Vector _lightSource;
     
-    public ShapesWithLightSourceRayTracer(List<Shape> shapes, Camera camera, Vector lightSource)
+    public ShapesWithLightSourceRayTracer(IEnumerable<Shape> shapes, Camera camera, Vector lightSource)
     {
         _shapes = shapes;
         _camera = camera;
         _lightSource = lightSource;
     }
         
-    public char Trace(Point point)
+    public TraceResult Trace(Point origin, Vector direction)
     {
-        var cameraToPoint = point - _camera.Origin;
-            
-        var nearestShape = _shapes.MinBy(s => s.GetDistanceTo(point));
+        // var cameraToPoint = point - _camera.Origin;
 
-        var hasIntersection = nearestShape.GetIntersectionWith(_camera.Origin, cameraToPoint) != null;
+        Intersection? nearestIntersection = null;
+        
+        foreach (var shape in _shapes)
+        {
+            var shapeIntersection = shape.GetIntersectionWith(origin, direction); //TODO: pass nearestIntersection to abort early inside GetIntersectionWith
+            // if (shapeIntersection != null)
+            // {
+            //     if (nearestIntersection?.Distance > shapeIntersection.Distance) continue;
+            //     nearestIntersection = shapeIntersection;
+            // }
             
-        if (hasIntersection)
-        {
-            return cameraToPoint.GetUnitVector().Dot(_lightSource) switch
+            // Console.WriteLine("D " + nearestIntersection?.Distance);
+            
+            if (
+                shapeIntersection != null && 
+                (shapeIntersection.Distance < nearestIntersection?.Distance || nearestIntersection == null))
             {
-                < 0 => ' ',
-                >= 0 and <= 0.2 => '.',
-                > 0.2 and <= 0.5 => '*',
-                > 0.5 and <= 0.8 => '0',
-                > 0.8 => '#',
-                _ => throw new ArgumentOutOfRangeException()
-            };
+                nearestIntersection = shapeIntersection;
+            }
         }
-        else
-        {
-            return ' ';
-        }
+
+        // if (point.X == 0 && point.Y == 0)
+        // {
+        //     Console.WriteLine(nearestIntersection.Point + " " + nearestIntersection.IsFound());
+        // }
+        
+        return TraceResult.FromIntersectionAndLightSource(nearestIntersection, _lightSource);
+            
+        // if (hasIntersection)
+        // {
+        //     return cameraToPoint.GetUnitVector().Dot(_lightSource) switch
+        //     {
+        //         < 0 => ' ',
+        //         >= 0 and <= 0.2 => '.',
+        //         > 0.2 and <= 0.5 => '*',
+        //         > 0.5 and <= 0.8 => '0',
+        //         > 0.8 => '#',
+        //         _ => throw new ArgumentOutOfRangeException()
+        //     };
+        // }
+        // else
+        // {
+        //     return ' ';
+        // }
     }
 }
--- src/Core/Scenery/Scene.cs
+++ src/Core/Scenery/Scene.cs
@@ -6,75 +6,38 @@ namespace Core.Scenery;
 public class Scene
 {
     public Camera Camera { get; private set; }
-    public Screen Screen { get; private set; }
+    // public Screen Screen { get; private set; }
     // public Plane Viewpoint { get; private set; }
-    private Vector RGB_Gray = Vector.FromXYZ(255, 255, 255);
+   
     
     // TODO: initialize from constructor
     // public Point? LightSource { get; private set; }
     // public List<Shape> Shapes { get; private set; }
 
-    public Scene(Camera camera, Screen screen)
+    public Scene(Camera camera)
     {
         Camera = camera;
-        Screen = screen;
     }
 
-    public void RayTrace(IRayTracer rayTracer)
+    public Image RayTrace(IRayTracer rayTracer)
     {
-        Screen.Reset();
+        var image = Image.FromResolution(Camera.SizeInPixels);
         
-        var viewpointCenter = Camera.Origin + Camera.Direction * Camera.Distance;
-
-        // TODO: scale image
-        var leftX = viewpointCenter.X - Screen.Height / 2.0;
-        var leftY = viewpointCenter.Y - Screen.Width / 2.0;
+        // Console.WriteLine(Camera.GetPointOfPixel(0, 0));
+        // Console.WriteLine(Camera.GetPointOfPixel(Camera.SizeInPixels - 1, Camera.SizeInPixels - 1));
         
-        for (var i = 0; i < Screen.Height; i++)
+        for (var i = 0; i < Camera.SizeInPixels; i++)
         {
-            for (var j = 0; j < Screen.Width; j++)
+            for (var j = 0; j < Camera.SizeInPixels; j++)
             {
                 // Currently viewpoint and points are both hardcoded
-                var point = Point.FromXYZ(leftX + i, leftY + j, viewpointCenter.Z);
-                var tracingResult = rayTracer.Trace(point);
-                Screen.SetChar(i, j, tracingResult);
+                var ray = Camera.GetRayOfPixel(i, j);
+                image[i, j] = rayTracer.Trace(Camera.Origin, ray);
             }
         }
-        
-        Screen.Draw();
+
+        return image;
     }
     
-    public void GenerateRayTracePicture(String outputFile)
-    {
-        var writer = new StreamWriter(outputFile);
-        writer.WriteLine("P3");
-        writer.WriteLine($"{Screen.Width}  {Screen.Height}");
-        writer.WriteLine("255");
-        for (var x = 0; x < Screen.Height; x++)
-        {
-            for (var y = 0; y < Screen.Width; y++)
-            {
-                switch (Screen.Chars[x, y])
-                {
-                    case ' ':
-                        writer.WriteLine((int)(RGB_Gray * 0).X + " " + (int)(RGB_Gray * 0).Y + " " + (int)(RGB_Gray * 0).Z);
-                        break;
-                    case '.':
-                        writer.WriteLine((int)(RGB_Gray * 0.1).X + " " + (int)(RGB_Gray * 0.1).Y + " " + (int)(RGB_Gray * 0.1).Z);
-                        break;
-                    case '*':
-                        writer.WriteLine((int)(RGB_Gray * 0.35).X + " " + (int)(RGB_Gray * 0.35).Y + " " + (int)(RGB_Gray * 0.35).Z);
-                        break;
-                    case '0':
-                        writer.WriteLine((int)(RGB_Gray * 0.65).X + " " + (int)(RGB_Gray * 0.65).Y + " " + (int)(RGB_Gray * 0.65).Z);
-                        break;
-                    case '#':
-                        writer.WriteLine((int)(RGB_Gray * 1).X + " " + (int)(RGB_Gray * 1).Y + " " + (int)(RGB_Gray * 1).Z);
-                        break;
-                }
-            }
-        }
-        writer.Flush();
-        writer.Close();
-    }
+    
 }
